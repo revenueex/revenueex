904337c80cb65bebfc7e307fd97744fc4ca2c036
Haidong Li <haidong.li@ca.com>
2016-10-16 20:44:01 -0500

    added recommendations

--- src/app/dashboard/recommendations/recommendations.component.ts
+++ src/app/dashboard/recommendations/recommendations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-recommendations',
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecommendationsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  recommendations: [any] = [
+    {
+      action: "email",
+      customer: "Customer ABC Inc",
+      solutions: [
+        "Offer 10% discount on product family 1 up to max 25,000 units",
+        "Offer to pull in order #123 by 7 days"
+      ],
+      ago: "5 min ago",
+      date: "2016-10-16T18:25:43.511Z"
+    },
+    {
+      action: "call",
+      customer: "Customer DEF Inc",
+      solutions: [
+        "Offer 10% discount on product family 1 up to max 25,000 units"
+      ],
+      ago: "45 min ago",
+      date: "2016-10-16T18:25:43.511Z"
+    }
+  ];
 
   ngOnInit() {
   }
