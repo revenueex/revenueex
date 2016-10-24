a9fe7f9afe9ae4ce32c57e969b8bc4594b873fbe
Haidong Li <haidong.li@ca.com>
2016-10-23 22:32:46 -0500

    dash line for future dates

--- src/app/dashboard/revenue-trend/revenue-trend.component.ts
+++ src/app/dashboard/revenue-trend/revenue-trend.component.ts
@@ -18,11 +18,13 @@ export class RevenueTrendComponent implements OnInit {
   ngOnInit() {
     var tmax = [56, 61, 67, 74, 82, 70, 72, 72, 64, 74, 77, 87, 73, 71, 71, 70, 81, 68, 75, 69, 62, 79, 68, 74, 77, 76, 74, 78, 80, 78, 79];
     var tmin = [47, 43, 46, 50, 55, 54, 51, 53, 58, 58, 62, 65, 55, 51, 53, 58, 61, 57, 57, 54, 52, 54, 51, 53, 57, 60, 61, 60, 63, 62, 63];
-    var tavg = [52, 52, 57, 62, 69, 62, 62, 63, 61, 66, 70, 76, 64, 61, 62, 64, 71, 63, 66, 62, 57, 67, 60, 64, 67, 68, 68, 69, 72, 70, 71];
-    var tmax2 = [56, 61, 67, 74, 82, 70, 72, 72, 64, 74, 77, 87, 73, 71, 71, 70, 81, 68, 75, 69, 62, 79, 68, 74, 77, 76, 74, 78, 80, 78, 79];
-    var tmin2 = [47, 43, 46, 50, 55, 54, 51, 53, 58, 58, 62, 65, 55, 51, 53, 58, 61, 57, 57, 54, 52, 54, 51, 53, 57, 60, 61, 60, 63, 62, 63];
+    var tavg = [52, 52, 57, 62, 69, 62, 62, 63, 61, 66, 70, 76, 64, 61, 62, 64, 71, 63, 66, 62, 57, 67, 60, 64, 76, 68, 68, 69, 72, 70, 71];
 
     var markerRange = [];
+    let startOfMonth: Date = new Date();
+    let dayOfMonth = startOfMonth.getDate();
+    startOfMonth.setDate(1);
+    startOfMonth.setHours(0, 0, 0);
 
     for (var n = 0; n < tavg.length; n++) {
       markerRange.push([tmin[n], tmax[n]]); // Push the [min, max] for each index
@@ -50,7 +52,23 @@ export class RevenueTrendComponent implements OnInit {
         "data-max": [56, 61, 67, 74, 82, 70, 72, 72, 64, 74, 77, 87, 73, 71, 71, 70, 81, 68, 75, 69, 62, 79, 68, 74, 77, 76, 74, 78, 80, 78, 79],
         tooltip: {
           visible: false
-        }
+        },
+        marker: {
+          visible: false
+        },
+        "aspect": "spline",
+        "rules": [
+          {
+            "rule": "%n >= " + dayOfMonth,
+            "line-style": "dashed",
+            "line-width": "2",
+          },
+          {
+            "rule": "%n < " + dayOfMonth,
+            "line-style": "solid",
+            "line-width": "2",
+          }
+        ]
       },
       scaleY: {
         format: "%v",
@@ -71,7 +89,7 @@ export class RevenueTrendComponent implements OnInit {
         }
       },
       scaleX: {
-        minValue: 1430438400000,
+        minValue: startOfMonth.getTime(),
         step: "day",
         lineWidth: 1,
         lineColor: "#2D2D2D",
@@ -115,7 +133,7 @@ export class RevenueTrendComponent implements OnInit {
         {
           type: "range",
           values: markerRange,
-          backgroundColor: "rgba(127, 127, 255, 0.8)",
+          backgroundColor: "rgba(128,222,234, 0.6)",
           lineWidth: 0,
           marker: {
             visible: false
