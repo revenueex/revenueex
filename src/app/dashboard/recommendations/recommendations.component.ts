4e1fca97d4349d630ca9628493ec39f0e270a8b7
Haidong Li <haidong.li@ca.com>
2016-11-05 22:09:45 -0500

    new recommendations

--- src/app/dashboard/recommendations/recommendations.component.ts
+++ src/app/dashboard/recommendations/recommendations.component.ts
@@ -12,24 +12,59 @@ export class RecommendationsComponent implements OnInit {
 
   recommendations: [any] = [
     {
-      action: "email",
-      customer: "Customer ABC Inc",
-      solutions: [
-        "Offer 10% discount on product family 1 up to max 25,000 units",
-        "Offer to pull in order #123 by 7 days"
-      ],
-      ago: "5 min ago",
-      date: "2016-10-16T18:25:43.511Z"
+      total: 4500,
+      items: [
+        {
+          customer: "Customer 1",
+          value: 1500,
+          suggestion: "Offer 10% discount"
+        },
+        {
+          customer: "Customer 2",
+          value: 3000,
+          suggestion: "Pull-in orders"
+        }
+      ]
+
+    }, {
+      total: 24000,
+      items: [
+        {
+          customer: "Customer 3",
+          value: 22000,
+          suggestion: "Offer Substitutions"
+        }
+      ]
+    }, {
+      total: 150000,
+      items: [
+        {
+          customer: "Customer 1",
+          value: 15000,
+          suggestion: "Email to Confirm"
+        },
+        {
+          customer: "Customer 3",
+          value: 10000,
+          suggestion: "Pull-in orders"
+        }
+      ]
+    }, {
+      total: 650000,
+      items: [
+        {
+          customer: "Customer 1",
+          value: 1500,
+          suggestion: "Offer 10% discount"
+        },
+        {
+          customer: "Customer 2",
+          value: 3000,
+          suggestion: "Pull-in orders"
+        }
+      ]
     },
-    {
-      action: "call",
-      customer: "Customer DEF Inc",
-      solutions: [
-        "Offer 10% discount on product family 1 up to max 25,000 units"
-      ],
-      ago: "45 min ago",
-      date: "2016-10-16T18:25:43.511Z"
-    }
+
   ];
 
   ngOnInit() {
